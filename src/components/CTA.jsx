4535dbf41c98d59a3f2fec38a257556d8c93e979
Julian Montes <julianmontesPS4@gmail.com>
2023-02-15 00:12:10 -0500

    Up till footer

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles';
+import Button from './Button';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={styles.heading2}>Lets try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus dicta iure quidem enim nisi at blanditiis.</p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button styles={`rounded-[20px]`}/>
+      </div>
+    </section>
   )
 }
 
--- src/components/FeedbackCrad.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FeedbackCrad = () => {
-  return (
-    <div>FeedbackCrad</div>
-  )
-}
-
-export default FeedbackCrad
